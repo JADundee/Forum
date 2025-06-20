563ed8d6023f7a4818f005c81f7e59f6beb2a5d3
JADundee <jadundee@icloud.com>
2025-06-20 08:38:22 -0600

    removed home button from footer

--- src/components/DashFooter.js
+++ src/components/DashFooter.js
@@ -1,36 +1,14 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faHouse } from "@fortawesome/free-solid-svg-icons"
-import { useNavigate, useLocation } from 'react-router-dom'
 import useAuth from "../hooks/useAuth"
 
 const DashFooter = () => {
 
     const {username, status} = useAuth()
 
-    const navigate = useNavigate()
-    const { pathname } = useLocation()
-
-    const onGoHomeClicked = () => navigate('/dash')
-
-    let goHomeButton = null
-    if (pathname !== '/dash') {
-        goHomeButton = (
-            <button
-                className="dash-footer__button icon-button"
-                title="Home"
-                onClick={onGoHomeClicked}
-            >
-                <FontAwesomeIcon icon={faHouse} />
-            </button>
-        )
-    }
-
-        const date = new Date()
-        const today = new Intl.DateTimeFormat('en-US', { dateStyle: 'full' }).format(date)
+    const date = new Date()
+    const today = new Intl.DateTimeFormat('en-US', { dateStyle: 'full' }).format(date)
 
     const content = (
         <footer className="dash-footer">
-            {goHomeButton}
             <p>Current User: {username}</p>
             <p>Permissions: {status}</p>
             <p className="dash-footer-date">{today}</p>
