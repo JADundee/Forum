232659564905bd961ea1534b3838aff1e3922692
JADundee <jadundee@icloud.com>
2025-08-21 11:53:49 -0600

    Ready to deploy

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -3,7 +3,7 @@ import { setCredentials } from "../../features/auth/authSlice";
 
 // Base query setup for all API requests, including attaching auth token if present
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:3500",
+  baseUrl: "https://forum-ahrf.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
